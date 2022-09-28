1abbfbff2e050c8fbf7e73d4ab40bb422c5f8c31
Brandon Reich <brandonwreich@gmail.com>
2022-09-27 20:56:13 -0600

    Create function stubs for Pathfinder.h

--- Lab4/Pathfinder.cpp
+++ Lab4/Pathfinder.cpp
@@ -1,1 +1,25 @@
 #include "Pathfinder.h"
+
+std::string Pathfinder::toString() {
+
+    return "";
+}
+
+void Pathfinder::createRandomMaze() {
+
+}
+
+bool Pathfinder::importMaze(std::string) {
+
+    return true;
+}
+
+std::vector<std::string> Pathfinder::solveMaze() {
+
+    return solution;
+}
+
+bool Pathfinder::findMazePath() {
+
+    return true;
+}
