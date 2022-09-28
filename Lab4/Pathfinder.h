58b708094b6ae5f2d50e09ffce4ae5a7e7fd1098
Brandon Reich <brandonwreich@gmail.com>
2022-09-27 20:53:05 -0600

    Create pathfinder.h file

--- Lab4/Pathfinder.h
+++ Lab4/Pathfinder.h
@@ -1,7 +1,36 @@
+#pragma once
+
+// Imports
 #include "PathfinderInterface.h"
 
-class Pathfinder: public PathfinderInterface {
-    public:
-        Pathfinder() {}
-        ~Pathfinder() {}
-}
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+// Global Variables
+const int ROW_SIZE = 5;
+const int COLUMN_SIZE = 5;
+const int BACKGROUND = 1;
+const int WALL = 0;
+const int TEMPORARY = 2; // Used to show this path has been explored
+const int PATH = 3;
+
+class Pathfinder : public PathfinderInterface {
+public:
+    // Constructors
+    Pathfinder() {}
+    ~Pathfinder() {}
+
+    // Member functions
+    std::string toString();
+    void createRandomMaze();
+    bool importMaze(std::string file_name);
+    std::vector<std::string> solveMaze();
+
+protected:
+    bool findMazePath(int grid[ROW_SIZE][COLUMN_SIZE], int r, int c);
+    int maze_grid[ROW_SIZE][COLUMN_SIZE];
+    std::vector<std::string> solution;
+};
